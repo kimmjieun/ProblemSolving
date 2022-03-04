83c30aaee4a12cc6aa858031fca22248e596020f
lee nam kyeong <skarod0401@naver.com>
2022-03-04 15:52:30 +0900

    [pgm] 43162 네트워크 AC

---
+++ namkyeong/2022/march/week1/P_43162.java
@@ -0,0 +1,30 @@
+package march.week1;
+
+public class P_43162 {
+    class Solution {
+        public int solution(int n, int[][] computers) {
+            int answer = 0;
+            boolean[] visited = new boolean[n];
+
+            for (int i = 0; i < n; i++) {
+                if (visited[i] == false) {
+                    answer++;
+                    dfs(i, visited, computers);
+                }
+            }
+
+            return answer;
+        }
+
+        public void dfs(int node, boolean[] visited, int[][] computers) {
+            visited[node] = true;
+
+            for (int i = 0; i < computers.length; i++) {
+                if (visited[i] == false && computers[node][i] == 1) {
+                    dfs(i, visited, computers);
+                }
+            }
+        }
+
+    }
+}
