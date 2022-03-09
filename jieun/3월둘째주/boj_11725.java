63d5b2dbd88c60ac0b88f5ba69f05062c134cd92
kimmjieun <kimmjieun@gmail.com>
2022-03-09 20:06:15 +0900

    [boj] 11725 트리의 부모 찾기 AC

--- jieun/3월둘째주/boj_11725.java
+++ jieun/3월둘째주/boj_11725.java
@@ -5,83 +5,46 @@ import java.util.*;
 
 public class boj_11725 {
     public static void main(String[] args) throws IOException {
-        // 인접 리스트
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
 
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for (int i = 0; i <= n; i++) // 0부터 초기화 해야함
-            list.add(new ArrayList<>());
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            arr.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            list.get(x).add(y);
-            list.get(y).add(x);
-
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr.get(a).add(b);
+            arr.get(b).add(a);
         }
 
-        boolean[] visit = new boolean[n + 1];
-        int[] result = new int[n + 1];
-
         Queue<Integer> q = new LinkedList<>();
+        boolean[] visit = new boolean[n + 1];
+        int[] parent = new int[n + 1];
+
         q.add(1);
         visit[1] = true;
 
         while (!q.isEmpty()) {
             int v = q.poll();
-            for (int i = 0; i < list.get(v).size(); i++) {
-                int tmp = list.get(v).get(i);
-                if (!visit[tmp]) {
-                    visit[tmp] = true;
-                    result[tmp] = v;
-                    q.add(tmp);
+            for (int next : arr.get(v)) {
+                if (!visit[next]) {
+                    visit[next] = true;
+                    parent[next] = v;
+                    q.add(next);
                 }
             }
         }
 
         for (int i = 2; i <= n; i++) {
-            sb.append(result[i] + "\n");
+           sb.append(parent[i]+"\n");
         }
         System.out.println(sb);
-
-        // 인접 행렬
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
-//        int n = Integer.parseInt(br.readLine());
-//
-//        int[][] arr = new int[n + 1][n + 1];
-//        for (int i = 0; i < n-1; i++) {
-//            StringTokenizer st = new StringTokenizer(br.readLine());
-//            int x = Integer.parseInt(st.nextToken());
-//            int y = Integer.parseInt(st.nextToken());
-//            arr[x][y] = arr[y][x] = 1;
-//        }
-//
-//        boolean[] visit = new boolean[n+1];
-//        int[] result = new int[n+1];
-//
-//        Queue<Integer> q = new LinkedList<>();
-//        q.add(1);
-//        visit[1]=true;
-//
-//        while(!q.isEmpty()){
-//            int tmp = q.poll();
-//            for(int i=1;i<=n;i++){
-//                if((arr[tmp][i]==1||arr[i][tmp]==1) && !visit[i]){
-//                    q.add(i);
-//                    result[i]=tmp;
-//                    visit[i]=true;
-//                }
-//            }
-//        }
-//        for(int i=2;i<=n;i++){
-//            sb.append(result[i]+"\n");
-//        }
-//        System.out.println(sb);
 
     }
 }
